dcb7bbcf186ab4dafab6073de15a0a516a04dfcc
Innovatech Solutions <innovatechsolutionsng@gmail.com>
2026-07-10 07:41:52 +0100

    Authentication fix2

--- assets/js/manager-setup.js
+++ assets/js/manager-setup.js
@@ -1,5 +1,4 @@
-const isLocalDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:' || window.location.hostname === '';
-const API_BASE = window.API_BASE !== undefined ? window.API_BASE : (isLocalDevelopment ? 'http://localhost:4000' : '');
+const API_BASE = window.API_BASE !== undefined ? window.API_BASE : ((window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:' || window.location.hostname === '') ? 'http://localhost:4000' : '');
 const managerSetupForm = document.getElementById("managerSetupForm");
 const setupMessage = document.getElementById("setupMessage");
 const setupStatusMessage = document.getElementById("setupStatusMessage");
