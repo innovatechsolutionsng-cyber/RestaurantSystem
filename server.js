6660ab51b1bdb0d40ff410bfada52b0609e4f78a
innovatechsolutionsng-cyber <innovatechsolutionsng@gmail.com>
2026-07-10 06:52:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -146,6 +146,19 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.static(__dirname));
 
+//Front-end Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "login.html"));
+});
+
+app.get("/manager-setup", (req, res) => {
+    res.sendFile(path.join(__dirname, "manager-setup.html"));
+});
+
 function createToken(payload) {
   return jwt.sign(payload, process.env.JWT_SECRET || "supersecretkey", { expiresIn: "3h" });
 }
